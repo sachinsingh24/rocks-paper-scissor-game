3775a9070a16e228062f0751b824f55b3c85f7f0
sachinsingh24 <sachinsingh24@rocketmail.com>
2021-09-27 11:21:03 +0530

    update ui

--- index.js
+++ index.js
@@ -1,46 +1,67 @@
-let game = ['Rocks', 'Paper', 'Scissor'];
-let result = [
+const game = ['Rocks', 'Paper', 'Scissor', 'lizard', 'spock'];
+const result = [
   'Result = Match Draw',
-  'Result = You Win Computer Lose',
-  'Result = You Lose Computer Win',
+  'Result = You Win',
+  'Result = Computer Win',
 ];
-const chalk = require('chalk');
-console.log(
-  chalk.white.bgBlue.bold(
-    `Enter values in numbers 1 for "Rocks", 2 for "Paper", 3 for "Scissor" :`
-  )
-);
-//collect result scores in array
+const userScore = document.getElementById('userScore');
+const computerScore = document.getElementById('computerScore');
+const userSelection = document.getElementById('userSelection');
+const computerSelection = document.getElementById('computerSelection');
+const result1 = document.getElementById('result');
+
+//Primary variable
+const clicked = [];
 let draw = 0,
   win = 0,
   lose = 0;
-// play 10 users
-for (let i = 0; i < 10; i++) {
+
+//perform click function and display results
+const button = (click1) => {
+  clicked.push(click1);
   const data = comp_choice();
-  console.log(chalk.white.bold(data));
-  if (data == result[0]) {
+  // display winner
+  if (win == 10) {
+    document.getElementById('pantagon').style.visibility = 'hidden';
+    document.getElementById('selection').style.visibility = 'hidden';
+    document.getElementById('end').style.visibility = 'visible';
+    document.getElementById('winner').innerText = `Game winner is user`;
+  } else if (lose == 10) {
+    document.getElementById('selection').style.visibility = 'hidden';
+    document.getElementById('pantagon').style.visibility = 'hidden';
+    document.getElementById('end').style.visibility = 'visible';
+    document.getElementById('winner').innerText = `Game winner is computer`;
+  } else if (data == result[0]) {
     draw++;
+    result1.innerText = result[0];
   } else if (data == result[1]) {
     win++;
+    userScore.innerText = win;
+    result1.innerText = result[1];
   } else {
     lose++;
+    computerScore.innerText = lose;
+    result1.innerText = result[2];
   }
-}
-console.log(
-  `Count Draw : ${draw}\nCount Win : ${win}\nCount Lose : ${lose
-  }\nTotal User Played : ${draw+win+lose}`);
+  clicked.shift();
+};
+  
 //this function for computer choose random values and compare with users value
 function comp_choice() {
-  const user_val = get_user();
-  console.log(chalk.white.bgRed.bold(`You choose : "${user_val}"`));
+  const user_val = clicked[0];
+  userSelection.innerText = `You choose : "${user_val}"`;
   //computer select random values and compare with users value
-  const computer = (game.computer = Math.floor(Math.random() * 3));
-  console.log(chalk.white.bgBlue.bold(`Computer choose : "${game[computer]}"`));
+  const computer = (game.computer = Math.floor(Math.random() * 5));
+  computerSelection.innerText = `Computer choose : "${game[computer]}"`;
   if (computer == 0) {
     //computer choose "rocks"
     if (game[0] == user_val) {
       return result[0];
     } else if (game[1] == user_val) {
+      return result[1];
+    } else if (game[2] == user_val) {
+      return result[2];
+    } else if (game[3] == user_val) {
       return result[1];
     } else {
       return result[2];
@@ -51,39 +72,51 @@ function comp_choice() {
       return result[2];
     } else if (game[1] == user_val) {
       return result[0];
+    } else if (game[2] == user_val) {
+      return result[1];
+    } else if (game[3] == user_val) {
+      return result[2];
     } else {
       return result[1];
     }
-  } else {
+  } else if (computer == 2) {
     //computer choose "Scissor"
     if (game[0] == user_val) {
       return result[1];
     } else if (game[1] == user_val) {
       return result[2];
+    } else if (game[2] == user_val) {
+      return result[0];
+    } else if (game[3] == user_val) {
+      return result[1];
+    } else {
+      return result[2];
+    }
+  } else if (computer == 3) {
+    //computer choose "Lizard"
+    if (game[0] == user_val) {
+      return result[2];
+    } else if (game[1] == user_val) {
+      return result[1];
+    } else if (game[2] == user_val) {
+      return result[2];
+    } else if (game[3] == user_val) {
+      return result[0];
+    } else {
+      return result[1];
+    }
+  } else {
+    //computer choose "Spock"
+    if (game[0] == user_val) {
+      return result[1];
+    } else if (game[1] == user_val) {
+      return result[2];
+    } else if (game[2] == user_val) {
+      return result[1];
+    } else if (game[3] == user_val) {
+      return result[2];
     } else {
       return result[0];
     }
-  }
-}
-//this function is for return user's value
-function get_user() {
-  //input user value in number 1 = rocks, 2 = paper, 3 = scissor.
-  // const user = parseInt(
-  //   prompt(
-  //     ` Enter values in numbers 1 for "Rocks", 2 for "Paper", 3 for "Scissor" : `
-  //   )
-  // );
-  const input = require('readline-sync');
-  const user = input.keyIn(); // optional variable if prompt error
-  if (user == 1) {
-    return game[0];
-  } else if (user == 2) {
-    return game[1];
-  } else if (user == 3) {
-    return game[2];
-  } else {
-    // alert(`enter only number 1 or 2 or 3`);
-    console.log(`User Take default value 3 : "${game[2]}"`);
-    return game[2];
   }
 }
