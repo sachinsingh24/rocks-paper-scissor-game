06597c4a6efe6ce87a085251ca0f8644008d5c82
sachinsingh24 <sachinsingh24@rocketmail.com>
2021-09-27 11:48:02 +0530

    scores update

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const computerScore = document.getElementById('computerScore');
 const userSelection = document.getElementById('userSelection');
 const computerSelection = document.getElementById('computerSelection');
 const result1 = document.getElementById('result');
+const pantagon = document.getElementById('pantagon');
+const selection = document.getElementById('selection');
+const end = document.getElementById('end');
+const winner = document.getElementById('winner');
 
 //Primary variable
 const clicked = [];
@@ -16,38 +20,27 @@ let draw = 0,
   win = 0,
   lose = 0;
 
-//perform click function and display results
-const button = (click1) => {
-  clicked.push(click1);
-  const data = comp_choice();
-  // display winner
-  if (win == 10) {
-    document.getElementById('pantagon').style.visibility = 'hidden';
-    document.getElementById('selection').style.visibility = 'hidden';
-    document.getElementById('end').style.visibility = 'visible';
-    document.getElementById('winner').innerText = `Game winner is user`;
-  } else if (lose == 10) {
-    document.getElementById('selection').style.visibility = 'hidden';
-    document.getElementById('pantagon').style.visibility = 'hidden';
-    document.getElementById('end').style.visibility = 'visible';
-    document.getElementById('winner').innerText = `Game winner is computer`;
-  } else if (data == result[0]) {
-    draw++;
-    result1.innerText = result[0];
-  } else if (data == result[1]) {
-    win++;
-    userScore.innerText = win;
-    result1.innerText = result[1];
-  } else {
-    lose++;
-    computerScore.innerText = lose;
-    result1.innerText = result[2];
-  }
-  clicked.shift();
+// functions
+
+const forUser = () => {
+  win++;
+  userScore.innerText = win;
+  pantagon.style.visibility = 'hidden';
+  selection.style.visibility = 'hidden';
+  end.style.visibility = 'visible';
+  winner.innerText = `Game winner is user`;
 };
-  
+const forComputer = () => {
+  lose++;
+  computerScore.innerText = lose;
+  selection.style.visibility = 'hidden';
+  pantagon.style.visibility = 'hidden';
+  end.style.visibility = 'visible';
+  winner.innerText = `Game winner is computer`;
+};
+
 //this function for computer choose random values and compare with users value
-function comp_choice() {
+const comp_choice = () => {
   const user_val = clicked[0];
   userSelection.innerText = `You choose : "${user_val}"`;
   //computer select random values and compare with users value
@@ -119,4 +112,28 @@ function comp_choice() {
       return result[0];
     }
   }
-}
+};
+
+//perform click function and display results
+const button = (click1) => {
+  clicked.push(click1);
+  const data = comp_choice();
+  // display winner
+  if (win == 9 && data == result[1]) {
+    forUser();
+  } else if (lose == 9 && data == result[2]) {
+    forComputer();
+  } else if (data == result[0]) {
+    draw++;
+    result1.innerText = result[0];
+  } else if (data == result[1]) {
+    win++;
+    userScore.innerText = win;
+    result1.innerText = result[1];
+  } else {
+    lose++;
+    computerScore.innerText = lose;
+    result1.innerText = result[2];
+  }
+  clicked.shift();
+};
